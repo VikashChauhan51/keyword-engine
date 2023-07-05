80930368974e80458db6b677cbb0117859885b47
Vikash Chauhan <vikashchauhan51@gmail.com>
2023-07-05 13:49:33 +0530

    New: Selenium example

---
+++ src/samples/KeywordEngine.Selenium/KeywordEngine.Selenium/Pages/SearchPage.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace KeywordEngine.Selenium.Pages;
+public class SearchPage
+{
+    private readonly IWebDriver driver;
+    private IWebElement searchTermInput => driver.FindElement(By.Id("sb_form_q"));
+    public SearchPage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public SearchPage Goto()
+    {
+        driver.Url = "https://www.bing.com";
+        return this;
+    }
+
+    public SearchPage Search(string text)
+    {
+        searchTermInput.SendKeys(text);
+        searchTermInput.SendKeys(Keys.Enter);
+
+        return this;
+    }
+}
